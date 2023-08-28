34c676c025bd2c909f43ae45160d2e076caf0e15
Sergei Mikurov <454004@mail.ru>
2023-08-28 19:11:17 +0500

    fix test_page_loader

--- tests/test_page_loader.py
+++ tests/test_page_loader.py
@@ -1,11 +1,42 @@
+import os
+import requests_mock
+import tempfile
+
 from page_loader import page_loader
 
 
 class TestPageLoader:
-    url_for_path = 'https://ru.hexlet.io/courses'
-    path_dir = '/var/tmp'
+    current_path = os.path.dirname(__file__)
+    file_name = 'ru-hexlet-io-courses.html'
+    expected_path = os.path.join(current_path, 'fixtures', file_name)
+    page_url = 'https://ru.hexlet.io/courses'
+    path_temp = '/var/tmp'
 
     def test_create_path(self):
         path_assert = '/var/tmp/ru-hexlet-io-courses.html'
-        path_created = page_loader.create_path(self.url_for_path, self.path_dir)
+        path_created = page_loader.create_path(self.page_url, self.path_temp)
         assert path_assert == path_created
+
+    def test_download(self):
+        with open(self.expected_path, 'r') as page:
+            data = page.read()
+        with requests_mock.Mocker() as mock:
+            mock.get(self.page_url, text=data)
+        with tempfile.TemporaryDirectory() as directory:
+            page_loader.download(self.page_url, directory)
+            expected_path = os.path.join(directory, self.file_name)
+        with open(expected_path) as file:
+            file_data = file.read()
+        assert data == file_data
+
+
+
+
+# __file__ - это путь к файлу, из которого загружен модуль.
+# Соответственно, если не пользоваться данной конструкцией,
+# то при обращении из разных каталогов,
+# он будет принимать разные значения.
+# # И он не всегда содержит абсолютный путь к файлу.
+# Использование __file__ в сочетании с различными модулями os.path
+# позволяет всем путям относиться к местоположению каталога текущего модуля.
+# Это позволяет переносить ваши модули / проекты на другие компьютеры.
