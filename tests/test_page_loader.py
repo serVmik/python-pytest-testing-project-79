0bcf09e31af102b098b481e2ac9ccea3f74f3be0
Sergei Mikurov <454004@mail.ru>
2023-08-28 22:06:22 +0500

    fix test, add test_create_file_path

--- tests/test_page_loader.py
+++ tests/test_page_loader.py
@@ -7,10 +7,18 @@ class TestPageLoader:
     page_url = 'https://ru.hexlet.io/courses'
     current_path = os.path.dirname(__file__)
     expected_file_name = 'ru-hexlet-io-courses.html'
-    path_to_file_with_expected_page = os.path.join(current_path, 'fixtures', expected_file_name)
+    path_to_file_with_expected_page = os.path.join(
+        current_path, 'fixtures', expected_file_name
+        )
     temp_path = '/var/tmp'
     expected_path = '/var/tmp/ru-hexlet-io-courses.html'
 
     def test_create_file_name(self):
         file_name = page_loader.create_file_name(self.page_url)
         assert self.expected_file_name == file_name
+
+    def test_create_file_path(self):
+        file_path = page_loader.create_file_path(
+            self.temp_path, self.expected_file_name
+            )
+        assert self.expected_path == file_path
