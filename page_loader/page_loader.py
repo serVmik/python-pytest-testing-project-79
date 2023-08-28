e5e48c53bc788d48174dcb0aa614a7da1fa7f804
Sergei Mikurov <454004@mail.ru>
2023-08-28 19:29:09 +0500

    fix test_page_loader

--- page_loader/page_loader.py
+++ page_loader/page_loader.py
@@ -1,6 +1,7 @@
-def create_path(url, path):
-    pass
+import re
 
 
-def download(page_url, directory):
-    pass
+def create_file_name(page_url: str) -> str:
+    _, url_without_schema = page_url.split('//')
+    file_name = re.sub('[^a-z0-9]', '-', url_without_schema.lower()) + '.html'
+    return file_name
--- tests/test_page_loader.py
+++ tests/test_page_loader.py
@@ -1,6 +1,4 @@
 import os
-import requests_mock
-import tempfile
 
 from page_loader import page_loader
 
@@ -8,34 +6,11 @@ from page_loader import page_loader
 class TestPageLoader:
     page_url = 'https://ru.hexlet.io/courses'
     current_path = os.path.dirname(__file__)
-    path_temp = '/var/tmp'
-    file_name = 'ru-hexlet-io-courses.html'
-    expected_path = os.path.join(current_path, 'fixtures', file_name)
-    def test_create_path(self):
-        path_assert = '/var/tmp/ru-hexlet-io-courses.html'
-        path_created = page_loader.create_path(self.page_url, self.path_temp)
-        assert path_assert == path_created
+    expected_file_name = 'ru-hexlet-io-courses.html'
+    path_to_file_with_expected_page = os.path.join(current_path, 'fixtures', expected_file_name)
+    temp_path = '/var/tmp'
+    expected_path = '/var/tmp/ru-hexlet-io-courses.html'
 
-    def test_download(self):
-        with open(self.expected_path, 'r') as page:
-            data = page.read()
-        with requests_mock.Mocker() as mock:
-            mock.get(self.page_url, text=data)
-        with tempfile.TemporaryDirectory() as directory:
-            page_loader.download(self.page_url, directory)
-            expected_path = os.path.join(directory, self.file_name)
-        with open(expected_path) as file:
-            file_data = file.read()
-        assert data == file_data
-
-
-
-
-# __file__ - это путь к файлу, из которого загружен модуль.
-# Соответственно, если не пользоваться данной конструкцией,
-# то при обращении из разных каталогов,
-# он будет принимать разные значения.
-# # И он не всегда содержит абсолютный путь к файлу.
-# Использование __file__ в сочетании с различными модулями os.path
-# позволяет всем путям относиться к местоположению каталога текущего модуля.
-# Это позволяет переносить ваши модули / проекты на другие компьютеры.
+    def test_create_file_name(self):
+        file_name = page_loader.create_file_name(self.page_url)
+        assert self.expected_file_name == file_name
