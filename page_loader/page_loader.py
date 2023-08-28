8121846424f884d4b2230e7e76c1e010135fed1c
Sergei Mikurov <454004@mail.ru>
2023-08-29 02:25:55 +0500

    fix page_loader.py

--- page_loader/page_loader.py
+++ page_loader/page_loader.py
@@ -1,5 +1,11 @@
 import re
 import os
+import requests
+
+
+def gat_page_data(page_url: str) -> str:
+    page_data = requests.get(page_url).text
+    return page_data
 
 
 def create_file_name(page_url: str) -> str:
@@ -8,6 +14,19 @@ def create_file_name(page_url: str) -> str:
     return file_name
 
 
-def create_file_path(temp_path: str, expected_file_name: str) -> str:
-    path: str = os.path.join(temp_path, expected_file_name)
-    return path
+def create_file_path(directory: str, file_name: str) -> str:
+    file_path = os.path.join(directory, file_name)
+    return file_path
+
+
+def save_page_data(page_data: str, file_path: str) -> None:
+    with open(file_path, 'w') as f:
+        f.write(page_data)
+
+
+def download(page_url: str, directory: str):
+    page_data = gat_page_data(page_url)
+    file_name = create_file_name(page_url)
+    file_path = create_file_path(directory, file_name)
+    save_page_data(page_data, file_path)
+    return file_path
