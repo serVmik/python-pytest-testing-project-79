60c991c4056f6b021c3b022e27e849c95dfb38c7
Sergei Mikurov <454004@mail.ru>
2023-08-28 22:21:07 +0500

    fix page_loader.py

--- page_loader/page_loader.py
+++ page_loader/page_loader.py
@@ -1,7 +1,13 @@
 import re
+import os
 
 
 def create_file_name(page_url: str) -> str:
     _, url_without_schema = page_url.split('//')
     file_name = re.sub('[^a-z0-9]', '-', url_without_schema.lower()) + '.html'
     return file_name
+
+
+def create_file_path(temp_path: str, expected_file_name: str) -> str:
+    path: str = os.path.join(temp_path, expected_file_name)
+    return path
