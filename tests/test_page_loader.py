3f9b4d1010d7b43508859cebf42231d559cdd2a4
Sergei Mikurov <454004@mail.ru>
2023-08-29 20:02:54 +0500

    add tests 2 step

--- tests/test_page_loader.py
+++ tests/test_page_loader.py
@@ -48,3 +48,35 @@ class TestDownload:
                 with open(temp_file_path) as fc:
                     current_data = fc.read()
                 assert current_data == expected_data
+
+
+class TestDownloadImages:
+    page_url = 'https://ru.hexlet.io/courses'
+    path_page_before_download = './tests/fixtures/before_download.html'
+    path_page_after_download = './tests/fixtures/after_download.html'
+    expected_dir_name = 'ru-hexlet-io-courses_files/'
+    expected_img_name = 'ru-hexlet-io-assets-professions-python.png'
+    expected_img_path = os.path.join(expected_dir_name, expected_img_name)
+
+    def test_download_img(self):
+        pass
+
+    def test_download(self):
+        with open(self.path_page_before_download) as fb:
+            page_data_before_download: str = fb.read()
+        with open(self.path_page_after_download) as fa:
+            page_data_after_download = fa.read()
+        with tempfile.TemporaryDirectory() as temp_dir:
+            with requests_mock.Mocker() as mock:
+                mock.get(self.page_url, text=page_data_before_download)
+                temp_path_file_html = pg_loader.download(self.page_url,
+                                                         temp_dir)
+                # does temp_html exists
+                assert os.path.exists(temp_path_file_html) is True
+                # does temp_img exists
+                temp_img_path = os.path.join(temp_dir, self.expected_img_path)
+                assert os.path.exists(temp_img_path)
+                # does change page_data
+                with open(temp_path_file_html) as fc:
+                    current_data = fc.read()
+                assert current_data == page_data_after_download
