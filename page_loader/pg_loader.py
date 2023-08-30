e0588ed5fe15f28429e553153a454921a2f6105b
Sergei Mikurov <454004@mail.ru>
2023-08-31 04:24:12 +0500

    refactoring

--- page_loader/pg_loader.py
+++ page_loader/pg_loader.py
@@ -5,64 +5,56 @@ from bs4 import BeautifulSoup
 from urllib.parse import urlparse
 
 
-def get_paths(page_url, download_dir=None, resource_dir=None):
-    """Creates directories and paths for downloaded content and resources"""
-    def change_view(view):
-        return re.sub('[^a-z0-9]', '-', view.lower()).rstrip()
+def get_paths(page_url: str, download_dir=None, resource_name=None) -> dict:
+    """Creates dirs and paths for downloaded content and resources"""
 
-    download_dir = '' if not download_dir else download_dir
-    resource_dir = '' if not resource_dir else resource_dir
+    def change_view(view: str) -> str:
+        return re.sub('[^a-z0-9]', '-', view.lower())
+
+    download_dir: str = '' if not download_dir else download_dir
+    resource_name: str = '' if not resource_name else resource_name
 
     _, base_url = page_url.split('//')
     base_path = os.path.join(download_dir, change_view(base_url))
     url_parse = urlparse(page_url)
     netloc_path = change_view(url_parse.netloc)
-    resource_dir = change_view(resource_dir).replace('-png', '.png')
+    resource_name, ext = os.path.splitext(resource_name)
+    resource_name = change_view(resource_name)
 
     return {
-        'download_dir': download_dir,
-        'local_page_path': f'{base_path}.html',
-        'local_resources_dir': f'{base_path}_files',
-        'local_img_path': f'{base_path}_files/{netloc_path}{resource_dir}',
+        'page_path': f'{base_path}.html',
+        'resources_dir': f'{base_path}_files',
+        'resource_path': f'{base_path}_files/{netloc_path}{resource_name}{ext}',
     }
 
 
-def replace_attr(page_content: str, tag: str, attr: str, page_url: str) -> str:
-    """Replaces attributes in the web page tags"""
-    soup = BeautifulSoup(page_content, 'html.parser')
+def replace_attr(page_data: str, tag: str, attr: str, page_url: str) -> str:
+    """Replaces the value of attributes in the web page tags"""
+
+    soup = BeautifulSoup(page_data, 'html.parser')
     img_tags = soup.find_all(tag)
 
     for img_tag in img_tags:
         img_tag[attr] = get_paths(
             page_url,
-            resource_dir=img_tag[attr]
-        )['local_img_path']
+            resource_name=img_tag[attr]
+        )['resource_path']
 
-    changed_page_content = soup.prettify()
-    return changed_page_content
-
-
-def save_page_data(page_data: str, file_path: str) -> None:
-    """Save html to file path"""
-    with open(file_path, 'w') as f:
-        f.write(page_data)
+    changed_page_data = soup.prettify()
+    return changed_page_data
 
 
-def create_dir_local_resources(dir_name):
-    os.mkdir(dir_name)
+def download(page_url: str, download_dir: str) -> str:
+    """Downloads the page and its resources, saves them locally"""
 
-
-def download(page_url: str, given_dir: str) -> str:
-    # Get paths
-    paths = get_paths(page_url, given_dir)
-    file_path = paths.get('local_page_path')
-
-    # Create dir for local resources
-    # os.mkdir(file_path)
-
-    # Get content for save
-    page_data: str = requests.get(page_url).text
+    paths = get_paths(page_url, download_dir)
+    page_path = paths.get('page_path')
+    page_data = requests.get(page_url).text
     changed_page_content = replace_attr(page_data, 'img', 'src', page_url)
-    save_page_data(changed_page_content, file_path)
 
-    return file_path
+    with open(paths['page_path'], 'w') as f:
+        f.write(changed_page_content)
+
+    os.mkdir(paths['resources_dir'])
+
+    return page_path
--- tests/test_pg_loader.py
+++ tests/test_pg_loader.py
@@ -1,4 +1,3 @@
-import os
 import requests_mock
 import tempfile
 from bs4 import BeautifulSoup
@@ -8,63 +7,50 @@ from page_loader import pg_loader
 
 class TestDownloadImages:
     page_url = 'https://ru.hexlet.io/courses'
-    path_page_before = './tests/fixtures/before_download.html'
-    path_page_after = './tests/fixtures/after_download.html'
-    expected_dir_name = 'ru-hexlet-io-courses_files'
-    expected_img_name = 'ru-hexlet-io-assets-professions-python.png'
-    expected_img_path = os.path.join(expected_dir_name, expected_img_name)
-    img_src_before = '/assets/professions/python.png'
-    img_src_after = 'ru-hexlet-io-courses_files/ru-hexlet-io-assets-professions-python.png'
+    download_dir = '/var/tmp'
+    page_path = '/var/tmp/ru-hexlet-io-courses.html'
+    resources_dir = '/var/tmp/ru-hexlet-io-courses_files'
+    resource_path = ('/var/tmp/ru-hexlet-io-courses_files/'
+                     'ru-hexlet-io-assets-professions-python.png')
 
-    def test_create_dir_name(self):
-        dir_name = pg_loader.get_paths(self.page_url)['local_resources_dir']
-        assert self.expected_dir_name == dir_name
+    fixture_loaded_page_data = './tests/fixtures/before_download.html'
+    fixture_saved_page_data = './tests/fixtures/after_download.html'
 
-    def test_create_dir(self):
-        with tempfile.TemporaryDirectory() as temp_dir_name:
-            paths = pg_loader.get_paths(self.page_url)
-            dir_name = paths['local_resources_dir']
-            current_dir_name = os.path.join(temp_dir_name, dir_name)
-            pg_loader.create_dir_local_resources(current_dir_name)
-            assert os.path.exists(current_dir_name) is True
+    loaded_tag_attr = '/assets/professions/python.png'
+    saved_tag_attr = '/var/tmp/ru-hexlet-io-courses_files/ru-hexlet-io-assets-professions-python.png'
 
-    def test_create_local_img_src(self):
-        current_img_src = pg_loader.get_paths(
+    def test_get_paths(self):
+        paths = pg_loader.get_paths(
             self.page_url,
-            resource_dir=self.img_src_before
-        )['local_img_path']
-        assert self.img_src_after == current_img_src
-
-    def test_replace_attr(self):
-        with open(self.path_page_before) as fpb:
-            page_content_before = fpb.read()
-            current_content = pg_loader.replace_attr(
-                page_content_before, 'img', 'src', self.page_url)
-            current_soup = BeautifulSoup(current_content, 'html.parser')
-            current_tags = current_soup.find_all('img')
-        with open(self.path_page_after) as fpa:
-            page_content_after = fpa.read()
-            expected_soup = BeautifulSoup(page_content_after, 'html.parser')
-            expected_tags = expected_soup.find_all('img')
-        assert expected_tags == current_tags
+            self.download_dir,
+            self.loaded_tag_attr
+        )
+        assert self.page_path == paths['page_path']
+        assert self.resources_dir == paths['resources_dir']
+        assert self.saved_tag_attr == paths['resource_path']
 
     def test_download(self):
-        with open(self.path_page_before) as fb:
-            page_data_before_download: str = fb.read()
-        with open(self.path_page_after) as fa:
-            page_data_after_download = fa.read()
-            page_after = BeautifulSoup(
-                page_data_after_download,
+        with open(self.fixture_loaded_page_data) as fb:
+            loaded_page_data: str = fb.read()
+        with open(self.fixture_saved_page_data) as fa:
+            saved_page_data = fa.read()
+            saved_page_data_prettify = BeautifulSoup(
+                saved_page_data,
                 'html.parser'
             ).prettify()
+
         with requests_mock.Mocker() as mock:
-            mock.get(self.page_url, text=page_data_before_download)
-            with tempfile.TemporaryDirectory() as directory:
-                temp_path_file_html = pg_loader.download(self.page_url, directory)
-                with open(temp_path_file_html) as fc:
-                    current_data = fc.read()
-                    page_current = BeautifulSoup(
-                        current_data,
+            mock.get(self.page_url, text=loaded_page_data)
+            with tempfile.TemporaryDirectory() as temp_dir:
+                path_current_page_data = pg_loader.download(
+                    self.page_url,
+                    temp_dir
+                )
+                with open(path_current_page_data) as fc:
+                    current_page_data = fc.read()
+                    current_page_data_prettify = BeautifulSoup(
+                        current_page_data,
                         'html.parser'
                     ).prettify()
-                assert page_current == page_after
+
+                assert current_page_data_prettify == saved_page_data_prettify
